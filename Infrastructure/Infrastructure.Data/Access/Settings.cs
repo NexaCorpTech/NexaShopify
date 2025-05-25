05875eb3a3c8d8aa98d12b9d762688b642b80312
IdrissaMoussaAdamou <idrissamoussaadamou544@gmail.com>
2025-05-25 18:37:15 +0200

    initial commit v2

--- Infrastructure/Infrastructure.Data/Access/Settings.cs
+++ Infrastructure/Infrastructure.Data/Access/Settings.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,15 +12,92 @@ namespace Infrastructure.Data.Access
     {
         public const int MAX_BATCH_SIZE = 1000;
         public static string ServerIP { get; set; }
+
         private static bool DebugMode = true;
 
+        #region>> Custom Method | If this region evolves, Transaction DB file also changes 
+
+        //Centralized management of connection chains
+        private static string _defaultConnectionString;
+        private static string _testConnectionString;
+        private static string _nexaCorpConnectionString;
+
+        public static string DefaultConnectionString => _defaultConnectionString;
+        public static string TestConnectionString => _testConnectionString;
+        public static string NexaCorpConnectionString => _nexaCorpConnectionString;
+
+
+        public static void SetDefaultConnectionString(string connectionString)
+        {
+            _defaultConnectionString = connectionString;
+        }
+        public static void SetNexaCorpConnectionString(string connectionString)
+        {
+            _nexaCorpConnectionString = connectionString;
+        }
+
+        public static void SetTestConnectionString(string connectionString)
+        {
+            _testConnectionString = connectionString;
+        }
+
+        #endregion
+
+        #region>> Commented section
+
+        //public static string ConnectionStringTest
+        //{
+        //    get
+        //    {
+        //        return _connectionStringTest;
+        //    }
+        //}
+
+        //public static string ConnectionString // Prop
+        //{
+        //    get
+        //    {
+        //        return _connectionString;
+        //    }
+        //}
+
+        //public static string ConnectionString_NexaCorp
+        //{
+        //    get
+        //    {
+        //        return _connectionStringNexa;
+        //    }
+        //}
+
+        //private static string _connectionString { get; set; }
+        //private static string _connectionStringTest { get; set; }
+        //private static string _connectionStringNexa { get; set; }
+        #endregion
+
+        #region>> Adda -4/2025 : it sucks, but I'll review it later, because I don't have time to think about securing our database (Nexa Corp) 
         public static string GetConnectionString()
         {
-            return @"Data Source=DESKTOP-VNSUCMM\SQLEXPRESS;Initial Catalog=Test;Integrated Security=True;Connect Timeout=30;Encrypt=True;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False";
+
+            return null;
         }
-        public static bool GetDebugMode()
+        //public static bool GetDebugMode()
+        //{
+        //    return DebugMode;
+        //}
+        #endregion
+        public class MyService
         {
-            return DebugMode;
+            private readonly IConfiguration _configuration;
+
+            public MyService(IConfiguration configuration)
+            {
+                _configuration = configuration;
+            }
+
+            public string GetConnectionString()
+            {
+                return _configuration.GetConnectionString("DefaultConnection");
+            }
         }
     }
 }
