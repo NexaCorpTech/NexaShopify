1c41e7c37236027bb7241a49cda6a569d4c0c9e6
IdrissaMoussaAdamou <idrissamoussaadamou544@gmail.com>
2025-05-25 19:32:01 +0200

    feat(AddPrimeVue)

--- WebFront/src/main.js
+++ WebFront/src/main.js
@@ -4,9 +4,16 @@ import router from "./router/pagesRouter";
 import '@fortawesome/fontawesome-free/css/all.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./scripts/imports";
+import PrimeVue from 'primevue/config';
+import Aura from '@primeuix/themes/aura';
 
 const app = createApp(App);
 
 app.use(router);
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura
+    }
+});
 
 app.mount("#app");
